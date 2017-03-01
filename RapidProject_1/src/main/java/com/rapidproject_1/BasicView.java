3e088c167f0a49f97ff49a7aeebf284b0b5cbcc5
Jordan <Jordan@Nerd-PC>
2017-03-01 13:16:05 -0500

    final commit of project

--- RapidProject_1/src/main/java/com/rapidproject_1/BasicView.java
+++ RapidProject_1/src/main/java/com/rapidproject_1/BasicView.java
@@ -2,11 +2,11 @@ package com.rapidproject_1;
 
 import com.gluonhq.charm.glisten.control.AppBar;
 import com.gluonhq.charm.glisten.control.Dialog;
+import com.gluonhq.charm.glisten.control.ProgressBar;
 import com.gluonhq.charm.glisten.mvc.View;
 import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -19,7 +19,9 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
@@ -49,14 +51,18 @@ public class BasicView extends View {
 
     private String ch, se, qu = "";
 
+    private String user = "";
+
     private Connection connection;
     private ResultSetMetaData resultsMeta;
     private ResultSet results;
-    private PreparedStatement insert;
     private Statement query;
     private String hint = "";
 
     private boolean correct = false;
+    private boolean showBack = true;
+
+    private int userid;
 
     public BasicView(String name) {
         super(name);
@@ -159,6 +165,7 @@ public class BasicView extends View {
     VBox questionView(String section) {
         VBox qv = new VBox();
         se = section;
+
         try {
             results = query.executeQuery("SELECT qnumber from question where qid = any(select qid from section_question where title=\"" + section + "\");");
             resultsMeta = results.getMetaData();
@@ -194,23 +201,115 @@ public class BasicView extends View {
         return qv;
     }
 
+    private int getCorrect() {
+        int correct = -1;
+        try {
+            results = query.executeQuery("select count(*) from user_question where userid in("
+                    + "select userid from user where name='" + user + "') and correct='true'");
+            if (results.next()) {
+                correct = Integer.parseInt(results.getString(1));
+            }
+        } catch (SQLException e) {
+
+        }
+        return correct;
+    }
+
+    private int getAttempts() {
+        int attempt = -1;
+        try {
+            results = query.executeQuery("select sum(attempts) from user_question where userid in("
+                    + "select userid from user where name='" + user + "') and correct='true'");
+            if (results.next()) {
+                attempt = Integer.parseInt(results.getString(1));
+            }
+        } catch (SQLException e) {
+
+        }
+        return attempt;
+    }
+
+    private int getRemaining() {
+        int total = -1;
+        try {
+            results = query.executeQuery("select count(*) from question");
+            if (results.next()) {
+                total = Integer.parseInt(results.getString(1));
+            }
+        } catch (SQLException e) {
+
+        }
+        return total - getCorrect();
+    }
+
+    private double getPercentComplete() {
+        double total = -1;
+        try {
+            results = query.executeQuery("select count(*) from question");
+            if (results.next()) {
+                total = Integer.parseInt(results.getString(1));
+                System.out.println(total);
+            }
+        } catch (SQLException e) {
+
+        }
+        return (total - getRemaining()) / total;
+    }
+
     VBox accountView() {
         VBox l = new VBox();
-        Label ul = new Label("THIS IS THE ACCOUNT VIEW");
 
-        l.getChildren().addAll(ul);
+        Label ul = new Label("User: " + user);
+        ul.setStyle("-fx-font-size: 25px; -fx-font-weight: bold;");
+        ul.setPadding(new Insets(10, 10, 10, 0));
+
+        HBox progressBar = new HBox();
+        double progress = getPercentComplete();
+        System.out.println(progress);
+
+        ProgressBar p = new ProgressBar();
+        p.setProgress(progress);
+        p.setPrefWidth(500);
+        p.setStyle("-fx-color: Green;");
+
+        Label a = new Label("Progress: ");
+        a.setStyle("-fx-font-size: 25px; -fx-font-weight: bold;");
+
+        Label b = new Label((int) (progress * 100) + "%");
+        b.setStyle("-fx-font-size: 25px; -fx-font-weight: bold;");
+
+        progressBar.getChildren().addAll(a, b);
+        progressBar.setAlignment(Pos.CENTER);
+
+        Label correctTitle = new Label("Correct:   " + getCorrect());
+        Label attemptTitle = new Label("Attempted: " + getAttempts());
+        Label remainTitle = new Label("Remaining:  " + getRemaining());
+
+        l.getChildren().addAll(ul, progressBar, p, correctTitle, attemptTitle, remainTitle);
+        l.setPadding(new Insets(100, 50, 50, 50));
+
         return l;
+    }
+
+    public void godMode() {
+        for (int i = 42; i < 360; i++) {
+            try {
+                query.executeUpdate("insert into user_question (userid, qid, attempts, correct) values(1, " + i + ", " + 1 + ", 'true')");
+            } catch (SQLException e) {
+
+            }
+        }
     }
 
     VBox login() {
         VBox l = new VBox();
-
-        TextField user = new TextField();
-        user.setMaxHeight(500);
+        // delete
+        TextField usern = new TextField();
+        usern.setMaxHeight(500);
         Label ul = new Label("Username:");
-        l.getChildren().addAll(ul, user);
+        l.getChildren().addAll(ul, usern);
 
-        TextField password = new TextField();
+        PasswordField password = new PasswordField();
         Label pl = new Label("Password:");
         l.getChildren().addAll(pl, password);
 
@@ -220,12 +319,13 @@ public class BasicView extends View {
             @Override
             public void handle(ActionEvent e) {
                 try {
-                    results = query.executeQuery("select * from user where name=\"" + user.getText() + "\" AND pw=\"" + password.getText() + "\"");
+                    results = query.executeQuery("select * from user where name=\"" + usern.getText() + "\" AND pw=\"" + password.getText() + "\"");
                     resultsMeta = results.getMetaData();
                     if (results.next()) {
+                        user = usern.getText();
                         setView(chapterView(), LOGIN_VIEW);
-                    }
-                    else {
+                        updateAppBar(RapidProject_1.getInstance().getAppBar());
+                    } else {
                         Label incorrect = new Label("incorrect user name or password");
                         l.getChildren().add(incorrect);
                     }
@@ -281,8 +381,12 @@ public class BasicView extends View {
                         resultsMeta = results.getMetaData();
                         if (!results.next()) {
                             query.executeUpdate("insert into user (name, pw) values ('" + usern.getText() + "', '" + rpassword.getText() + "');");
+                            user = usern.getText();
                             Label success = new Label("User " + usern.getText() + " registered successfully!");
                             l.getChildren().add(success);
+                            setView(chapterView(), LOGIN_VIEW);
+
+                            updateAppBar(RapidProject_1.getInstance().getAppBar());
                         } else {
                             Label taken = new Label("Username " + usern.getText() + " taken");
                             l.getChildren().add(taken);
@@ -328,8 +432,8 @@ public class BasicView extends View {
         // HANDLING CORRECT ANSWERS
         ToggleGroup group = new ToggleGroup();
 
-        ArrayList<RadioButton> radioAnswers = new ArrayList<>();
-        ArrayList<RadioButton> correctAnswers = new ArrayList<>();
+        ArrayList<CheckBox> radioAnswers = new ArrayList<>();
+        ArrayList<CheckBox> correctAnswers = new ArrayList<>();
 
         Button submitButton = new Button("Submit");
         submitButton.setOnAction(new EventHandler<ActionEvent>() {
@@ -342,23 +446,67 @@ public class BasicView extends View {
                 });
                 dialog.getButtons().add(okButton);
 
-                ArrayList<Toggle> selected = new ArrayList<Toggle>();
-                for (Toggle t : group.getToggles()) {
+                ArrayList<CheckBox> selected = new ArrayList<>();
+                for (CheckBox t : radioAnswers) {
                     if (t.isSelected()) {
                         selected.add(t);
                     }
                 }
-                if (correctAnswers.containsAll(selected)) {
+                // select attempts from user-question where userid =userid and qid = qid
+                int attempts = 0;
+                try {
+
+                    results = query.executeQuery("select attempts from user_question where userid in("
+                            + "select userid from user where name='" + user + "') and qid=" + Integer.parseInt(qe));
+
+                    if (results.next()) {
+                        attempts = Integer.parseInt(results.getString(1));
+                    } else {
+                        results = query.executeQuery("select userid from user where name='" + user + "'");
+                        if (results.next()) {
+                            int uid = Integer.parseInt(results.getString(1));
+                            query.executeUpdate("insert into user_question (userid, qid, attempts, correct) values(" + uid + ", " + Integer.parseInt(qe) + ", " + attempts + ", 'false')");
+                            System.out.println("insert");
+                        } else {
+                            System.out.println("SERIOUS ERROR");
+                        }
+                    }
+                    System.out.println("entry");
+
+                } catch (SQLException a) {
+                    a.printStackTrace();
+                }
+                attempts++;
+                if (correctAnswers.equals(selected)) {
+
+                    int qid = Integer.parseInt(qe);
+                    try {
+
+                        query.executeUpdate("update user_question set attempts=" + attempts + ", correct='true'"
+                                + " where userid in(select userid from user where name='" + user + "') and qid=" + qid);
+                    } catch (SQLException a) {
+                        a.printStackTrace();
+                    }
+
                     dialog.setContent(new Label("You're Correct!"));
                     dialog.showAndWait();
 
-                    int qid = Integer.parseInt(qe);
                     qid++;
                     setView(question("" + qid), QUESTION_VIEW);
 
+                    // insert into user-question (userid, qid, attemps, correct) values....
                 } else {
+                    try {
+                        int qid = Integer.parseInt(qe);
+                        query.executeUpdate("update user_question set attempts=" + attempts + ", correct='false'"
+                                + " where userid in(select userid from user where name='" + user + "') and qid=" + qid);
+                    } catch (SQLException a) {
+                        a.printStackTrace();
+                    }
+
                     dialog.setContent(new Label("Sorry, You're Wrong"));
                     dialog.showAndWait();
+
                 }
             }
         });
@@ -382,7 +530,7 @@ public class BasicView extends View {
             results = query.executeQuery("select aprompt, correct from answer where aid IN( select aid from question_answer where qid=" + qe + ");");
 
             while (results.next()) {
-                RadioButton b = new RadioButton(results.getString(1));
+                CheckBox b = new CheckBox(results.getString(1));
                 radioAnswers.add(b);
                 if (results.getString(2).contains("true")) {
                     System.out.println(results.getString(2));
@@ -391,10 +539,6 @@ public class BasicView extends View {
                 }
                 vbox.getChildren().add(b);
                 vbox.setAlignment(Pos.TOP_LEFT);
-            }
-
-            for (RadioButton b : radioAnswers) {
-                b.setToggleGroup(group);
             }
 
         } catch (SQLException e) {
@@ -441,46 +585,54 @@ public class BasicView extends View {
 
     @Override
     protected void updateAppBar(AppBar appBar) {
+        if (last == LOGIN_VIEW) {
+            appBar.setVisible(true);
+        } else {
+            appBar.setVisible(false);
 
-        /*ProgressBar p2 = new ProgressBar();
-         p2.setProgress(0.25F);
-         p2.setStyle("-fx-color: Green;");
-         p2.setPrefHeight(25);
+            /*ProgressBar p2 = new ProgressBar();
+             p2.setProgress(0.25F);
+             p2.setStyle("-fx-color: Green;");
+             p2.setPrefHeight(25);
         
-         appBar.getActionItems().add(p2);*/
-        appBar.setNavIcon(MaterialDesignIcon.ARROW_BACK.button(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
+             appBar.getActionItems().add(p2);*/
+            if (showBack) {
+                appBar.setNavIcon(MaterialDesignIcon.ARROW_BACK.button(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent e) {
 
-                switch (last) {
-                    case CHAPTER_VIEW:
-                        setView(chapterView(), CHAPTER_VIEW);
-                        break;
-                    case SECTION_VIEW:
-                        setView(sectionView(ch), CHAPTER_VIEW);
-                        break;
-                    case QUESTION_VIEW:
-                        setView(questionView(se), SECTION_VIEW);
-                        break;
-                    case VIEWQ_VIEW:
-                        break;
-                    case ACCOUNT_VIEW:
-                        break;
+                        switch (last) {
+                            case CHAPTER_VIEW:
+                                setView(chapterView(), CHAPTER_VIEW);
+                                break;
+                            case SECTION_VIEW:
+                                setView(sectionView(ch), CHAPTER_VIEW);
+                                break;
+                            case QUESTION_VIEW:
+                                setView(questionView(se), SECTION_VIEW);
+                                break;
+                            case VIEWQ_VIEW:
+                                break;
+                            case ACCOUNT_VIEW:
+                                break;
+                        }
+                    }
+                }));
+            }
+            appBar.setTitleText("Java Questions");
+            appBar.getActionItems().add(MaterialDesignIcon.PAGES.button(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent e) {
+                    setView(chapterView(), CHAPTER_VIEW);
                 }
-            }
-        }));
-        //appBar.setTitleText("Java Questions");
-        appBar.getActionItems().add(MaterialDesignIcon.PAGES.button(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                setView(chapterView(), CHAPTER_VIEW);
-            }
-        }));
-        appBar.getActionItems().add(MaterialDesignIcon.ACCOUNT_BOX.button(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                setView(accountView(), last + 1);
-            }
-        }));
+            }));
+            appBar.getActionItems().add(MaterialDesignIcon.ACCOUNT_BOX.button(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent e) {
+                    setView(accountView(), last + 1);
+                }
+            }));
+
+        }
     }
 }
